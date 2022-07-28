019803a03686fceac4a29d9e12f2512ced97a891
MirzaDgtu <mirzyali@mail.ru>
2022-07-28 12:21:23 +0700

    start develop methods GetMail v_0.0.2.0

--- Main.pas
+++ Main.pas
@@ -56,95 +56,81 @@ implementation
 uses SConsts;
 
 procedure TfmMain.actGetMailExecute(Sender: TObject);
-{var
-  SMTP: TidPOP3;
-  i: integer;
-begin
-  try
-  //  Msg.From.Address := sFromEmailAdress + ';mirzali.pirmagomedov@vostok-td.ru';
-    Msg.ContentType := 'multipart/related';        // Кодировка для русского языка
-    Msg.CharSet := 'Windows-1251';          // иначе будут ????? в письме
-    Msg.IsEncoded := True;
-
-    SMTP := TidPOP3.Create(nil);
-      try
-        try
-         SMTP.Host := 'pop.mail.ru';
-         SMTP.Port := 110;
-         SMTP.Username := 'reports@vostok-td.ru';
-         SMTP.Password := 'uaA2eAiRSo^2';
-
-         OpenSSL.Destination := SMTP.Host + ':' + IntToStr(SMTP.Port);
-         OpenSSL.Host := SMTP.Host;
-         OpenSSL.Port := SMTP.Port;
-         OpenSSL.DefaultPort := 0;
-         OpenSSL.SSLOptions.Mode := sslmUnassigned;
-
-         SMTP.IOHandler := OpenSSL;
-         SMTP.UseTLS := utUseExplicitTLS;
-
-         SMTP.Connect;
-         SMTP.Retrieve(SMTP.CheckMessages-1, Msg);
-
-         for I := 0 to Msg.MessageParts.Count-1 do
-           Begin
-             if Msg.MessageParts.Items[i] is TIdAttachmentFile then
-              Begin
-                if FileExists(TIdAttachmentFile(Msg.MessageParts.Items[i].FileName).FileName) then
-                  DeleteFile(TIdAttachmentFile(Msg.MessageParts.Items[i].FileName).FileName);
-                  TIdAttachmentFile(Msg.MessageParts.Items[i].FileName).SaveToFile(Msg.MessageParts.Items[i].FileName);
-              End;
-
-              if Msg.MessageParts.Items[i] is TIdText then
-                Begin
-                  memoLog.Lines.Add('Текст письма от ' + Msg.From.Address);
-                  memoLog.Lines.Add(TIdText(Msg.MessageParts.Items[i]).Body.Text);
-                End;
-           End;
-        except
-          on err: Exception do
-            memoLog.Lines.Add('Ошибка при отправке письма - ' + err.Message);
-        end;
-      finally
-        SMTP.Disconnect;
-      end;
-  finally
-
-  end;     }
-
 var
   MsgCount: integer;
   i, c: integer;
 Begin
   try
 
-    Msg.ContentType := 'multipart/related';        // Кодировка для русского языка
-    Msg.CharSet := 'Windows-1251';          // иначе будут ????? в письме
-    Msg.IsEncoded := True;
+    try                           
+      Msg.ContentType := 'multipart/related';        // Кодировка для русского языка
+      Msg.CharSet := 'Windows-1251';                 // иначе будут ????? в письме
+      Msg.IsEncoded := True;
 
-    POP.Connect;
-    MsgCount := POP.CheckMessages;
-    memoLog.Lines.Add('Количество писем - ' + MsgCount.ToString);
+      memoLog.Lines.Add('Попытка соединения...');
+      
+      POP.Connect;
+      MsgCount := POP.CheckMessages;
+      memoLog.Lines.Add('Соединение успешно установлено');
+    except
+      on ex: Exception do
+        Begin
+          memoLog.Lines.Add('Произошла ошибка подключения. Сообщение: ' + ex.Message);
+          Exit;
+        End;
+    end;
+     
 
     for I := 1 to MsgCount do
     try
       Msg.Clear;
       POP.Retrieve(i, Msg);
 
+
       if Msg.From.Address = 'subwoofer.666@yandex.ru' then
         Begin      
+          memoLog.Lines.Add('Письмо от ' + Msg.From.Address);          
           for c := 0 to Msg.MessageParts.Count-1 do
               try
-       //          if Msg.MessageParts.Items[c] is TIdAttachmentFile then
-       //           Begin
-       //             TIdAttachmentFile(Msg.MessageParts.Items[i].FileName).SaveToFile(ExtractFilePath(GetModuleName(0)) + 'In\' +  Msg.MessageParts.Items[i].FileName);
-       //            End;
+                 if Msg.MessageParts.Items[c] is TIdAttachmentFile then
+                  try
+                    if FileExists(ExtractFilePath(GetModuleName(0)) + 
+                                                  'In\' + 
+                                                  TIdAttachmentFile(Msg.MessageParts.Items[c]).FileName) then
+                      DeleteFile(ExtractFilePath(GetModuleName(0)) + 
+                                                 'In\' + 
+                                                 TIdAttachmentFile(Msg.MessageParts.Items[c]).FileName);
+                      
+                    TIdAttachmentFile(Msg.MessageParts.Items[c]).SaveToFile(ExtractFilePath(GetModuleName(0)) + 
+                                                                            'In\' +  
+                                                                            Msg.MessageParts.Items[c].FileName);
+
+                    memoLog.Lines.Add('Файл - ' + (ExtractFilePath(GetModuleName(0)) + 
+                                                                   'In\' +  
+                                                                   Msg.MessageParts.Items[c].FileName) + ' успешно сохранен');
+                  except
+                    on ex: Exception do
+                      Begin
+                        memoLog.Lines.Add('Ошибка сохранения файла - ' + 
+                                          TIdAttachmentFile(Msg.MessageParts.Items[c]).FileName + 
+                                          '. Сообщение: ' + ex.Message);
+                        Continue;
+                      End;
+                  End;
+
+                   
 
                  if Msg.MessageParts.Items[c] is TIdText then
                   Begin
-                    memoLog.Lines.Add('Текст письма от ' + Msg.From.Address);
-                    memoLog.Lines.Add('Тема письма - ' + Msg.Subject);
-                    memoLog.Lines.Add(TIdText(Msg.MessageParts.Items[c]).Body);
+                    try
+                      memoLog.Lines.Add('-----------------------------------');
+                      memoLog.Lines.Add('Текст письма от ' + Msg.From.Address);
+                      memoLog.Lines.Add('Тема письма - ' + Msg.Subject);
+                      memoLog.Lines.Add(TIdText(Msg.MessageParts.Items[c]).Body.Text);
+                      memoLog.Lines.Add('-----------------------------------');                    
+                    finally
+
+                    end;
                   End;
               except
                 on exc: Exception do
